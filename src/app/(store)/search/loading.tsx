1ef65dde36a5b800054a2ad3dca5c95ddef613f9
gcarpaneze <carpanezexls@gmail.com>
2023-12-01 06:04:02 -0300

    fix: ajust static pages

--- src/app/(store)/search/loading.tsx
+++ src/app/(store)/search/loading.tsx
@@ -1,17 +1,13 @@
-'use client'
-
 import { Skeleton } from '@/components/skeleton'
-import { useSearchParams } from 'next/navigation'
+import { CurrentSearch } from './current-search'
+import { Suspense } from 'react'
 
 export default function SearchLoading() {
-  const searchParams = useSearchParams()
-
-  const query = searchParams.get('q')
   return (
     <div className="flex flex-col gap-4">
-      <p className="text-sm">
-        Resultados para <span className="font-semibold">{query}</span>
-      </p>
+      <Suspense>
+        <CurrentSearch />
+      </Suspense>
 
       <div className="grid grid-cols-3 gap-6">
         <Skeleton className="row-span-1 h-[460px] rounded-lg bg-zinc-900" />
